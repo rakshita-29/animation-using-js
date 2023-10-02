a91e72d61e25839a11f426e0b25fd8b68b41818e
Rakshita <rakshitachoudhary2000@gmail.com>
2023-10-02 21:26:46 +0530

    ADD:background animation.

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 const board = document.getElementById("board");
+
+function createDot() {
+  const dot = document.createElement("div");
+  dot.className = "dot";
+  dot.style.width = "10px";
+  dot.style.height = "10px";
+  dot.style.left = Math.random() * window.innerWidth + "px";
+  dot.style.top = Math.random() * window.innerHeight + "px";
+  document.body.appendChild(dot);
+
+  dot.animate(
+    [
+      { transform: "translate(0, 0)" },
+      {
+        transform: `translate(${Math.random() * 200 - 100}px, ${
+          Math.random() * 200 - 100
+        }px)`,
+      },
+      { transform: "translate(0, 0)" },
+    ],
+    {
+      duration: Math.random() * 4000 + 3000, 
+      iterations: Infinity,
+      direction: "alternate",
+      easing: "ease-in-out",
+    }
+  );
+}
+
+function createDots(numDots) {
+  for (let i = 0; i < numDots; i++) {
+    createDot();
+  }
+}
+
+createDots(100);
