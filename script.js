f6de14f781dd649764e254fba7ea07b32ab0d4e9
Rakshita <rakshitachoudhary2000@gmail.com>
2023-10-03 10:44:39 +0530

    UPDATE:css and js

--- script.js
+++ script.js
@@ -68,17 +68,40 @@ function click(e) {
     return;
   }
 
+  current = current === "X" ? "O" : "X";
 }
 
 function win() {
- 
-  
+  const wincase = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (const pattern of wincase) {
+    const [a, b, c] = pattern;
+    if (
+      boxes[a].textContent &&
+      boxes[a].textContent === boxes[b].textContent &&
+      boxes[a].textContent === boxes[c].textContent
+    ) {
+      return true;
+    }
+  }
+
+  return false;
 }
 
 function draw() {
- 
+  return boxes.every((box) => box.textContent !== "");
 }
 
 function reset() {
- 
+  boxes.forEach((box) => (box.textContent = ""));
+  current = "X";
 }
